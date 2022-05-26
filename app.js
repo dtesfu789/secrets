4019a9cfee847f2703ea017b057eed803cdd01cf
dtesfu789 <ddytesfu@gmail.com>
2022-05-26 23:33:41 +0200

    Level 3 Hashing with md5 setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
 const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 
 const app = express();
@@ -15,7 +16,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
 const User = new mongoose.model("User", userSchema);
 
 
@@ -38,7 +39,7 @@ app.route("/login")
         }, function(err, docs) {
           if (!err) {
             if (docs) {
-              if (docs.password === req.body.password) {
+              if (docs.password === md5(req.body.password)) {
                 res.render("secrets");
               } else {
                 res.send("Your email and password did not match. Please try again.");
@@ -59,7 +60,7 @@ app.route("/register")
   .post(function(req, res) {
     const user = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
     user.save(function(err) {
       if (err) {
